c82c68eff81db140b199f1cb3d3ef045d979b480
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2016-08-05 01:37:05 +0530

    added app and server code

--- src/app/home/component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './component.html',
-  styleUrls: ['./component.css']
-})
-
-export class AppComponent { }
