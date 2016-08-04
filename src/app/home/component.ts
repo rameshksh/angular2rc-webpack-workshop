97e4dda4d7527af79f616c44d1824682c326d2f4
ramesh-sharma12 <ramesh.sharma12@gmail.com>
2016-08-04 19:57:07 +0530

    added basic project

---
+++ src/app/home/component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './component.html',
+  styleUrls: ['./component.css']
+})
+
+export class AppComponent { }
